41e9b0c6534a6a3462118e47811340c47842e913
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2024-08-23 22:35:47 +0530

    Time: 2 ms (43.76%), Space: 10.3 MB (19.91%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
-    void bubblesort(vector<int>& n){
-        for(int i = 0; i < n.size()-1; i++){
-            for(int j = 0;j < n.size()-1-i; j++){
-                if(n[j] > n[j+1]) swap(n[j],n[j+1]);
-            }
-        }
-    }
     void sortColors(vector<int>& nums) {
-        bubblesort(nums);
+        int i = 0,j=0, k = nums.size()-1;
+        while(j<=k){
+            if(nums[j]==0){
+                swap(nums[j],nums[i]);
+                i++;
+                j++;
+            }
+            else if(nums[j]==2){
+                swap(nums[j],nums[k]);
+                k--;
+                
+            }
+            else j++;
+        }
     }
 };
