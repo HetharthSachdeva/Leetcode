05dbd3b636de435f22057ede029a4940bbf1f16f
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2024-08-23 19:43:10 +0530

    Time: 7 ms (84.85%), Space: 16 MB (6.92%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         unordered_map<int,int> m;
         for(int i =0; i < nums.size(); i++){
             m[nums[i]] = i;
