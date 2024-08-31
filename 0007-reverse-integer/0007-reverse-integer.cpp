a97d6a8152590f14553a77ac845326824c69216f
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2024-08-31 18:18:27 +0530

    Time: 0 ms (100%), Space: 8.4 MB (39.59%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
     int reverse(int x) {
-       long long rev = 0;
+       int rev = 0;
        signed l = INT_MIN;
        signed r = INT_MAX;
         if(x<0){
             while(x<0){
+            if(rev>(r/10) || rev<(l/10))
+            return 0;    
             rev*=10;
             rev -= x%10;
             x/=10;
+            
+       
        }
-       if(rev>r || rev<l){
-            return 0;
-       }
+       
        return -rev;
         }
        while(x>0){
+            if(rev>(r/10) || rev<(l/10))
+            return 0;
             rev*=10;
             rev+=x%10;
             x/=10;
+            
+       
        }
-       if(rev>r || rev<l){
-            return 0;
-       }
+       
        return rev;
     }   
 };
+    
