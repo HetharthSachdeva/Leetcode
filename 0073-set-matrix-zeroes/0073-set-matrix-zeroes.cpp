bc4504b0854432bf394d76dfe5c6146073a507be
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2024-08-31 22:41:03 +0530

    Time: 8 ms (76.81%), Space: 17.3 MB (8.89%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         int n = matrix.size(), m = matrix[0].size();
         set<int> rows, cols;
         for (int i = 0; i < n; i++) {
