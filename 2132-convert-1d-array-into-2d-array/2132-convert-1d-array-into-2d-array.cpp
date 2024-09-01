a23e9b1b67e495002a8736f8099d99a223965608
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2024-09-01 18:39:57 +0530

    Time: 59 ms (99.1%), Space: 94.4 MB (15.95%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
-        cin.tie(NULL);
+        cout.tie(NULL);
         vector<vector<int>> ans(m);
         int s = original.size();
         if((m*n) != s) return {};
