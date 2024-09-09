899c3e1ed53aa9cecd620c9307ce438eb4110683
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2024-09-09 12:35:10 +0530

    Time: 8 ms (65.34%), Space: 18.7 MB (52.34%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -4,16 +4,32 @@ public:
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
         vector<int> ans;
-        map<int, int> f;
+        int c1 = 0, c2 = 0, e1 = 0, e2 = 0;
         for(int i: nums){
-            f[i]++;
-        }
-        int n = nums.size();
-        for(auto i: f){
-            if(i.second> n/3){
-                ans.push_back(i.first);
+            if(c1 == 0 && i!=e2){
+                c1++;
+                e1 = i;
+            }
+            else if(c2 == 0 && i!=e1){
+                c2++;
+                e2 = i;
+            }
+            else if(e1 == i) c1++;
+            else if(e2 == i) c2++;
+            else{
+                c1--;
+                c2--;
             }
         }
+        c1 = 0; c2 =0;
+        for(int j: nums){
+            if(j==e1) c1++;
+            else if(j==e2) c2++;
+        }
+        int n = nums.size();
+        if(c1>n/3) ans.push_back(e1);
+        if(c2> n/3) ans.push_back(e2);
         return ans;
+
     }
 };
