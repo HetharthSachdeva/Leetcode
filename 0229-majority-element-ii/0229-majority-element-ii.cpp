dccb6496d7d7335ed30f095bd43800d8ef18c007
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2024-09-09 12:15:43 +0530

    Time: 15 ms (10.75%), Space: 18.9 MB (38.32%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         vector<int> ans;
         map<int, int> f;
         for(int i: nums){
