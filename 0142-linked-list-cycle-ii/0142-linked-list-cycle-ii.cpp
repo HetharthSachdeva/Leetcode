28e34e9bcf901d09583ed3ec04fc18589d24d14b
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2025-01-08 16:17:55 +0530

    Time: 11 ms (18.71%), Space: 11.4 MB (23.28%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,18 +9,21 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode* ptr = head;
-        int ind = 0;
-        map<ListNode*,int> m;
-        while(ptr != NULL){
-            if(m.find(ptr)!=m.end()){
-                return ptr;
-            }
-            m[ptr] = ind;
-            ptr = ptr->next;
-            ind++;
+        if(!head || !head->next) return NULL;
+        ListNode* ptr1 = head, *ptr2 = head;
+        while(ptr2!=NULL && ptr2->next != NULL){
+            
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next->next;
+            if(ptr1==ptr2) break;
 
         }
-        return NULL;
+        if(!(ptr2) || !ptr2->next) return NULL;
+        ptr1 = head;
+        while(ptr1!=ptr2){
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
+        }
+        return ptr1;
     }
 };
