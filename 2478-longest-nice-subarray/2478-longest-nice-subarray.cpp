976914133d6b2ecdac9a28c93fa43a50d49d3af7
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2025-03-18 10:50:21 +0530

    Time: 4 ms (38.81%), Space: 60.8 MB (87.7%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    bool check(vector<int>& arr, int len, vector<long long>& sum, vector<long long>& xo){
-        for(int i = 0;i <= arr.size()-len; i++){
-            if((sum[i+len-1]-sum[i]+arr[i]) == (xo[i+len-1]^xo[i]^arr[i])) return true;
-        }
-        return false;
-    }
     int longestNiceSubarray(vector<int>& nums) {
         int n = nums.size();
-        long long xo=0;
-        long long sum = 0;
-        vector<long long> s(n), x(n);
-        for(int i = 0; i < n; i++){
-            sum+=nums[i];
-            xo^=nums[i];
-            s[i] = sum;
-            x[i] = xo;
-        }
-        int i = 1, j = min(n, 30);
-        int ans = 1;
-        while(i <= j){
-            int mid = i+(j-i)/2;
-            if(check(nums, mid, s, x)){
-                i=mid+1;
-                ans = mid;
-            } 
-            else j = mid-1;
+        int i = 0, j = 0, orr = 0, ans = 1;
+        while(j < n){
+            if((orr&nums[j]) != 0 && i < j){
+                orr^=(nums[i++]);
+                
+            }
+            else{
+                orr|=nums[j];
+                ans = max(ans, j-i+1);
+                j++;
+            }
         }
         return ans;
     }
