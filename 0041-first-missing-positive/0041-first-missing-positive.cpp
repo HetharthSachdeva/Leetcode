e225c3ef9b10389c75b0a34fdf81d54fc0321148
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2025-04-19 01:29:36 +0530

    Time: 0 ms (100%), Space: 55.2 MB (23.72%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        map<int, int> f;
         int n = nums.size();
-        for(int i = 0; i < n;i++){
-            f[nums[i]]++;
-        }
-        for(int i = 1; i < INT_MAX; i++){
-            if(f.find(i) == f.end()){
-                return i;
+        vector<bool> what(n, false); 
+        for(int i = 0; i < n; i++){
+            if(nums[i]<=n && nums[i]>=1){
+                what[nums[i]-1] = true;
             }
         }
-        return 1;
+        for(int i = 0; i < n; i++){
+            if(!what[i]) return i+1;
+        }
+        return n+1;
     }
 };
