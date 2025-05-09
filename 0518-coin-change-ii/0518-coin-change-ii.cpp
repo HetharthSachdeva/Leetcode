02ce85c1e2f8b4bf962c46d1ec4fe2f6222ae3ee
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2025-05-09 09:31:24 +0530

    Time: 0 ms (100%), Space: 9.7 MB (99.74%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-       vector<int> dp(amount+1, 0);
+       vector<unsigned int> dp(amount+1, 0);
        dp[0] = 1;
        for(int c: coins){
             for(int i = c; i < amount+1; i++){
-                if(dp[i] < INT_MAX-dp[i-c]) dp[i]= (dp[i]+dp[i-c]);
-                else dp[i] += (INT_MIN+dp[i-c]);
+                // if(dp[i] < INT_MAX-dp[i-c]) dp[i]= (dp[i]+dp[i-c]);
+                dp[i]= (dp[i]+dp[i-c]);
+                // else dp[i] += (INT_MIN+dp[i-c]);
             }
        }
        return dp[amount];
