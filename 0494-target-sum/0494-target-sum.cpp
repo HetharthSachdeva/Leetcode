e723dae34d4ddbaaf4b5358ca003c215f2fde012
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2025-05-27 14:33:22 +0530

    Time: 10 ms (63.92%), Space: 16.3 MB (39.38%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    int helper(vector<int>& arr, int t, int index, vector<vector<int>>& d){
-
-        if(index==arr.size()){
-            if(t==0) return 1;
-            else return 0;
-        }
-        if(d[t+2001][index]!=-1) return d[t+2001][index];
-        return d[t+2001][index] = helper(arr, t-arr[index], index+1, d)+helper(arr, t+arr[index], index+1, d);
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n= nums.size();
-        vector<vector<int>> dp(2*2000+1, vector<int>(nums.size(),-1));
-        return helper(nums, target, 0, dp);
+        int sum = 0;
+        for(int i:nums) sum+= i;
+        if(target > sum ) return 0;
+        if((sum-target) & 1) return 0;
+        vector<vector<int>> dp((sum-target)/2 + 1,vector<int>(n,0));
+        // for(int i = 0; i < n; i++){
+        //     dp[0][i]=1;
+        // }
+        dp[0][0] = 1;
+
+        if(nums[0] <= (sum-target)/2) dp[nums[0]][0] += 1;
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= (sum-target)/2; j++){
+                dp[j][i]+=dp[j][i-1];
+                if(j-nums[i]>=0) dp[j][i] += dp[j-nums[i]][i-1];
+            }
+        }
+
+        return dp[(sum-target)/2][n-1];
     }
 };
