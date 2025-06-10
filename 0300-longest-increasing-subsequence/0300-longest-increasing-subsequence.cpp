c1b4e20bb002ece3729d12fffac6b72364ce35a2
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2025-06-10 15:09:24 +0530

    Time: 0 ms (100%), Space: 13.9 MB (94.44%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int solve(vector<int>& arr, int ind, int prev, vector<vector<int>>& dp){
-        if(ind>=arr.size()){
-            return 0;
-        }
-
-        if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
-
-        int no = solve(arr, ind+1, prev, dp);
-        int take = 0;
-        if(prev==-1 || arr[prev]<arr[ind]) take = 1 + solve(arr, ind+1, ind, dp);
-
-        return dp[ind][prev+1] = max(no, take);
-    }
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n+1,-1));
-        return solve(nums, 0, -1, dp);       
+        vector<int> sub; int ans = 1;sub.push_back(nums[0]);
+        for(int i = 1; i < n; i++){
+            if(nums[i]>sub[sub.size()-1]){
+                sub.push_back(nums[i]);
+                if(ans<sub.size()) ans =  sub.size();
+            } 
+            else{
+                int a = 0, b = sub.size()-1;
+                while(a<=b){
+                    int mid = a+(b-a)/2;
+                    if(sub[mid]>=nums[i]) b=mid-1;
+                    else a = mid+1;
+                }
+                sub[a] = nums[i];
+            }
+        }     
+        return ans;
     }    
 };
