bab275287f713d69271d8b788c0508c836f5b652
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2025-06-28 15:40:57 +0530

    Time: 13 ms (46.64%), Space: 17.3 MB (89%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,34 +9,11 @@
  */
 class Solution {
 public:
-    vector<TreeNode*> path(TreeNode* root, TreeNode* target){
-        if(!root || !target) return {};
-        if(root==target){
-            return {target};
-        }
-
-        vector<TreeNode*> l = path(root->left, target);
-        vector<TreeNode*> r = path(root->right, target);
-        // if(l.size() == 0 && r.size()==0) return {};
-        vector<TreeNode*> v = {root};
-        if(r.size()){
-            v.insert(v.end(), r.begin(), r.end());
-            return v;
-        }
-        else if(l.size()){
-            v.insert(v.end(), l.begin(), l.end());
-            return v;
-        }
-        return {};
-
-    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> a = path(root, p), b = path(root, q);
-        TreeNode* ans;
-        for(int i= 0; i < min(a.size(), b.size()); i++){
-            if(a[i]==b[i]) ans = a[i];
-            else break;
-        }
-        return ans;
+        if(!root || root == p || root==q) return root;
+        TreeNode* l = lowestCommonAncestor(root->left,p,q),*r= lowestCommonAncestor(root->right,p,q); 
+        if(!l) return r;
+        if(!r) return l;
+        return root;
     }
 };
