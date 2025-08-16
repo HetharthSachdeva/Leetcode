93301462fc36a2d2b4b531774a779a90dc25bde0
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2025-08-16 09:04:12 +0530

    Time: 3 ms (73.17%), Space: 18.9 MB (26.62%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& list, vector<bool>& path, vector<bool>& visited, stack<int>& topo){
-        visited[node] = true;
-        path[node] = true;
-        for(auto i : list[node]){
-            if(path[i]) return false;
-            if(!visited[i]){
-                if((!dfs(i, list, path, visited, topo))) return false;
-            }
-        }
-        path[node] = false;
-        topo.push(node);
-        return true;
-    }
-
     vector<int> findOrder(int n, vector<vector<int>>& pre) {
-        stack<int> st;
-        vector<int> ans;
-        vector<bool> vis(n, false), path(n, false);
+        queue<int> q;
+        vector<int> indegree(n, 0), ans;
         vector<vector<int>> adj(n);
 
         for(auto i: pre){
             adj[i[1]].push_back(i[0]);
+            indegree[i[0]]++;
         }
 
         for(int i = 0; i < n; i++){
-            if(!vis[i]){
-                if(!dfs(i, adj, path, vis, st)) return {};
-            }
+            if(indegree[i] == 0) q.push(i);
         }
 
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
+        while(!q.empty()){
+            int k = q.front();
+            q.pop();
+            for(int i: adj[k]){
+                indegree[i]--;
+                if(indegree[i]==0) q.push(i);
+                if(indegree[i] < 0) return {};
+            }
+
+            ans.push_back(k);
         }
-        // reverse(ans.begin(), ans.end());
+        if(ans.size() < n) return {};
         return ans;
     }
 };
