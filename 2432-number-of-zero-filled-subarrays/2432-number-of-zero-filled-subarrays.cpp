b04cf516e55d1e8f252b2d2940b392afb2ee96c0
Hetharth Sachdeva <77833648+HetharthSachdeva@users.noreply.github.com>
2025-08-19 08:53:10 +0530

    Time: 3 ms (40.28%), Space: 111.4 MB (40.61%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -3,20 +3,21 @@ public:
     long long zeroFilledSubarray(vector<int>& nums) {
         long long ans = 0;
         int n = nums.size();
-        vector<int> j; int l = 0;
+        // vector<int> j; 
+        long long l = 0;
         for(int i = 0; i < n; i++){
             if(nums[i]==0) l++;
             else if(l>0){
-                j.push_back(l);
+                ans+= (l*(l+1))/2;
                 l=0;
             } 
             else l = 0;
         }
-        if(l>0) j.push_back(l);
+        if(l>0) ans+= (l*(l+1))/2;
 
-        for(long long i: j){
-            ans += (i*(i+1))/2;
-        }
+        // for(long long i: j){
+        //     ans += (i*(i+1))/2;
+        // }
         return ans;
     }
 };
